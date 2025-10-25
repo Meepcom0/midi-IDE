5617a920dab35b749f3a4faf8c0c357e2969915f
Ayden Coughlin <51276518+acou12@users.noreply.github.com>
2025-10-25 13:09:18 -0400

    add new types

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,30 @@ enum Mode {
   INT_CONSTANT,
 }
 
+enum NodeType {
+  PROGRAM,
+  BLOCK,
+  ADD,
+  ASSIGN,
+  IF,
+  GREATER_THAN,
+}
+
+type Node = {
+  type: NodeType;
+  parent: Node | undefined;
+  childIndex: number | undefined; // defined such that this == this.parent[childIndex]
+  children: Node[];
+};
+
 let mode = Mode.COMMAND;
+
+let currentNode: Node = {
+  type: NodeType.PROGRAM,
+  parent: undefined,
+  childIndex: undefined,
+  children: [],
+};
 
 function eventLoop(key: number): void {
   if (mode === Mode.COMMAND) {
@@ -16,3 +39,24 @@ function eventLoop(key: number): void {
     // ...
   }
 }
+
+// function onMIDIMessage(event) {
+//   let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+//   for (const character of event.data) {
+//     str += `0x${character.toString(16)} `;
+//   }
+//   console.log(str);
+// }
+
+// function startLoggingMIDIInput(midiAccess) {
+//   midiAccess.inputs.forEach((entry) => {
+//     entry.onmidimessage = onMIDIMessage;
+//   });
+// }
+
+// async function setupMidi() {
+//   await navigator.permissions.query({ name: "midi" });
+//   let midiAccess = await navigator.requestMIDIAccess();
+// }
+
+// setupMidi();
