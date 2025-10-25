5d65acef22ff7e599f08807811ecc29749045b19
Ayden Coughlin <51276518+acou12@users.noreply.github.com>
2025-10-25 12:00:56 -0400

    add event loop gunction

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import "./style.css";
+
+enum Mode {
+  COMMAND,
+  INT_CONSTANT,
+}
+
+let mode = Mode.COMMAND;
+
+function eventLoop(key: number): void {
+  if (mode === Mode.COMMAND) {
+    if (key === 12) {
+      mode = Mode.INT_CONSTANT;
+    }
+  } else if (mode === Mode.INT_CONSTANT) {
+    // ...
+  }
+}
